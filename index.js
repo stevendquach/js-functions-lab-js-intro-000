ba7c3ba49bd7d026297477f2b9143fd97945603a
Steven Quach <stevendquach@gmail.com>
2020-08-09 08:46:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function happyHolidaysTo(name) {
 function happyCustomHolidayTo(holiday, name) {
   return (`Happy ${holiday}, ${name}!`)
 }
+function holidayCountdown(days, holiday) {
+  return (`It's ${days} days until ${holiday}!`)
+}
