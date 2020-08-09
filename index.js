2fed2a92f6a847781885495faf6bb36bc43bddfa
Steven Quach <stevendquach@gmail.com>
2020-08-09 08:34:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,4 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return (`Happy holidays, ${name}!`)
 }
-function happyHolidaysTo(holiday, name) {
-  return (`Happy ${holiday}, ${name}!`)
-  }
-}  
+
